3a6be2066d2abaeb70fac951ecff81b6ea6526ab
Red <vhanchar@ucsc.edu>
2019-06-14 08:41:21 -0700

    RAWERSRARARWERSARARARRRR!

--- challenges/objects-arrays.js
+++ challenges/objects-arrays.js
@@ -5,30 +5,39 @@
   Use this pattern to create your objects: 
   object name, diet, weight, length, period
 */
-
+function Dinosaur(name, diet, weight, length, period){
+	this.name = name;
+	this.diet = diet;
+	this.weight = weight;
+	this.length = length;
+	this.period = period;
+}
 // tyrannosaurus, carnivorous, 7000kg, 12m, Late Cretaceous
+const tyro = new Dinosaur ("tyrannosaurus", "carnivorous", "7000kg", "12m", "Late Cretaceous"); 
 
 // stegosaurus, herbivorous, 2000kg, 9m, Late Jurassic
+const stego = new Dinosaur ("stegosaurus", "herbivorous", "2000kg", "9m", "Late Jurassic");
 
 // velociraptor, carnivorous, 15kg, 1.8m, Late Cretaceous
-
+const velo = new Dinosaur ("velociraptor", "carnivorous", "15kg", "1.8m", "Late Cretaceous");
 // Using your dinosaur objects, log answers to these questions:
 
 // How much did tyrannosaurus weigh?
-console.log();
+console.log(tyro.weight);
 
 // What was the diet of a velociraptor?
-console.log();
+console.log(velo.diet);
 
 // How long was a stegosaurus?
-console.log();
+console.log(stego.length);
 
 // What time period did tyrannosaurus live in?
-console.log();
+console.log(tyro.period);
 
 
 // Create a new roar method for the tyrannosaurus.  When called, return "RAWERSRARARWERSARARARRRR!" Log the result.
-console.log();
+Dinosaur.prototype.roar = function (typeRoar) {return typeRoar};
+console.log(tyro.roar("RAWERSRARARWERSARARARRRR!"));
 
 
 // ==== Arrays ====
