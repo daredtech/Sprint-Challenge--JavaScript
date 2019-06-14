6922ca6911ac532eea50df559cad53110b8d068f
Red <vhanchar@ucsc.edu>
2019-06-14 10:32:25 -0700

    consume add

--- challenges/functions.js
+++ challenges/functions.js
@@ -4,8 +4,12 @@
   * Create a higher-order function named consume that can take 3 parameters.
   * The first two parameters can accept any argument
   * The last parameter accepts a callback 
-  * In the body of the function return the callback with the two parameters that you created
+  * In the body of the function return the callback with the two parameters
+   that you created
 */
+function consume(param1, param2, cb){
+  return cb(param1, param2);
+}
 
 
 /* Step 2: Create several functions to callback with consume();
@@ -13,7 +17,16 @@
   * Create a function named multiply that returns the product of two numbers 
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
-
+function add (param1, param2){
+  return(param1 + param2);
+}
+// function multiply (){
+  
+// }
+// function greeting (){
+  
+// }
+console.log(consume(2,2,add)); // 4
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
 // consume(2,2,add); // 4
@@ -28,15 +41,15 @@
 // Explanation: 
 
 
-const external = "I'm outside the function";
+// const external = "I'm outside the function";
 
-function myFunction() {
-  console.log(external);
-  const internal = "Hello! I'm inside myFunction!";
+// function myFunction() {
+//   console.log(external);
+//   const internal = "Hello! I'm inside myFunction!";
 
-  function nestedFunction() {
-    console.log(internal);
-  };
-  nestedFunction();
-}
-myFunction();
+//   function nestedFunction() {
+//     console.log(internal);
+//   };
+//   nestedFunction();
+// }
+//myFunction();
